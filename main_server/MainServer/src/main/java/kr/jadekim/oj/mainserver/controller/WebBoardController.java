045ff9884f0d3b61fd49b1be704cce6fd9fc393e
Cheon Yu Jung <chyjis@naver.com>
2016-02-27 11:18:07 +0900

    no message

--- main_server/MainServer/src/main/java/kr/jadekim/oj/mainserver/controller/WebBoardController.java
+++ main_server/MainServer/src/main/java/kr/jadekim/oj/mainserver/controller/WebBoardController.java
@@ -1,17 +1,43 @@
 package kr.jadekim.oj.mainserver.controller;
 
+import kr.jadekim.oj.mainserver.entity.Post;
+import kr.jadekim.oj.mainserver.repository.PostRepository;
+import kr.jadekim.oj.mainserver.repository.ProblemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Map;
 
 /**
  * Created by cheonyujung on 2016. 2. 24..
  */
+@Controller
 public class WebBoardController {
-    @RequestMapping("/board")
-    public ModelAndView showBoard(){
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("board.tpl");
-        return modelAndView;
-    }
 
+    @Autowired
+    PostRepository postRepository;
+
+    @RequestMapping("/notice")
+    public ModelAndView showBoard(ModelAndView modelAndView, @PageableDefault(sort = { "id" }, size = 25) Pageable pageable){
+        ArrayList<Map> messages = new ArrayList<>();
+        Iterator<Post> post = (Iterator<Post>) postRepository.findAll(pageable);
+        for(Post p : post){
+
+        }
+        modelAndView.setViewName("board");
+
+        return new ModelAndView("board", "messages", messages);
+    }
+    @RequestMapping("/question")
+    public ModelAndView showQuestion(ModelAndView modelAndView, @PageableDefault(sort = { "id" }, size = 25) Pageable pageable){
+        ArrayList<Map> messages = new ArrayList<>();
+        modelAndView.setViewName("board");
+        return new ModelAndView("board", "messages", messages);
+    }
 }
--- main_server/MainServer/src/main/resources/templates/board.tpl
+++ main_server/MainServer/src/main/resources/templates/board.tpl
@@ -2,12 +2,40 @@ layout 'test.tpl', title: '게시판',
         content: contents {
             div(class: 'container') {
                 div(class: 'prob-nav') {
-                    input(type: 'button', value : '공지')
-                    input(type: 'button', value : 'Q&A')
+                    a(href : '/notice', '공지사항')
+
+                    a(href : '/question', 'Q&A')
                     form(id: 'search-form', action = '', method = 'post') {
                         input(type: 'text', value: '', name: 'search')
                         input(type: 'button', value: '검색')
                     }
+                }
+            }
+            div(class: 'prob-container') {
+                table(class: 'prob-table') {
+                     thead {
+                          tr {
+                              td '번호'
+                              td '제목'
+                              td '작성자'
+                              td '날짜'
+                          }
+                     }
+                     tbody {
+                         if (messages.empty) {
+                              tr { td(colspan: '5', 'No Problem') }
+                              } else {
+                                   messages.each { message ->
+                                   tr {
+                                       td message.id
+                                       td message.name
+                                       td message.count
+                                       td message.rate
+                                       td message.result
+                                   }
+                              }
+                         }
+                     }
                 }
             }
         }
