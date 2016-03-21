a23f3f838e6633900db13527a8b0ffc91d83c607
Jade Kim <jinyong@jadekim.kr>
2016-03-21 16:37:28 +0900

    [수정] install.sh에서 oracle jdk 에서 open jdk 로 변경

--- evaluation_server/src/main/resources/sh/install.sh
+++ evaluation_server/src/main/resources/sh/install.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
+
 sudo apt-get update
-sudo apt-get install oracle-java8-installer docker-engine mysql-server
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository -y ppa:openjdk-r/ppa
+sudo apt-get update
+sudo apt-get install openjdk-8-jdk docker-engine mysql-server
 sudo groupadd docker
 sudo gpasswd -a ${USER} docker
 sudo service restart docker
