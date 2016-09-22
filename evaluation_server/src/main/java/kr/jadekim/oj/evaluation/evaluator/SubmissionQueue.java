a8bddcbcf85a700c335eee7cbd07acea3df9eec4
OhYongTaek <ka123ak1@gmail.com>
2016-09-23 00:25:44 +0900

    [수정] evaluation server 수정

--- evaluation_server/src/main/java/kr/jadekim/oj/evaluation/evaluator/SubmissionQueue.java
+++ evaluation_server/src/main/java/kr/jadekim/oj/evaluation/evaluator/SubmissionQueue.java
@@ -1,14 +1,17 @@
 package kr.jadekim.oj.evaluation.evaluator;
 
+import com.sun.istack.internal.Nullable;
 import kr.jadekim.oj.evaluation.models.Submission;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.function.Consumer;
 
 /**
  * Created by jdekim43 on 2016. 2. 27..
  */
-public class SubmissionQueue {
+public class SubmissionQueue implements Iterable<Submission> {
 
     private static volatile SubmissionQueue instance;
 
@@ -29,11 +32,36 @@ public class SubmissionQueue {
         queue.add(submission);
     }
 
+    @Nullable
     public Submission next() {
         return queue.poll();
     }
 
     public int count() {
         return queue.size();
+    }
+
+    @Override
+    public Iterator<Submission> iterator() {
+        return new Iterator<Submission>() {
+
+            @Override
+            public boolean hasNext() {
+                return !queue.isEmpty();
+            }
+
+            @Override
+            @Nullable
+            public Submission next() {
+                return queue.poll();
+            }
+        };
+    }
+
+    @Override
+    public void forEach(Consumer<? super Submission> action) {
+        while(!queue.isEmpty()) {
+            action.accept(queue.poll());
+        }
     }
 }
