f87c6209ebc13c08aa33957ff69a590e08b9f9eb
Sun Yeong <vmffkxlgnqh1@gmail.com>
2016-09-26 11:42:48 +0900

    ContestList table수정, 그에따라 controller도 수정

--- main_server/MainServer/src/main/java/kr/jadekim/oj/mainserver/controller/WebController/WebContestListController.java
+++ main_server/MainServer/src/main/java/kr/jadekim/oj/mainserver/controller/WebController/WebContestListController.java
@@ -124,7 +124,11 @@ public class WebContestListController {
             Date startTime = c.getStartTime();
             Date endTime = c.getEndTime();
             if(startTime != null && endTime != null &&today.before(endTime)&&today.after(startTime)){
-                map.put("isgoing",true);
+                map.put("isgoing",1);
+                map.put("startTime",format.format(startTime));
+            }
+            else if(startTime != null && endTime != null &&today.before(startTime)){
+                map.put("isgoing",2);
                 map.put("startTime",format.format(startTime));
             }
             else{
